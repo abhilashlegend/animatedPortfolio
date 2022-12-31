50d3f59a8cc0f19ad51e02fc8dfc45e53b10e6ed
abhilashlegend <abhilash456a@live.com>
2022-12-31 21:57:18 +0530

    added sticky menu and progressbar

--- js/app.js
+++ js/app.js
@@ -47,11 +47,42 @@ const animateCircles = (e, x, y) => {
 
 }
 
+let hoveredElPosition = [];
+
+const stickyElement = (x, y, hoveredEl) => {
+    if(hoveredEl.classList.contains("sticky")){
+        if(hoveredElPosition.length < 1){
+            hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft];
+        }
+        hoveredEl.style.cssText = `top:${x}px; left: ${y}px`;
+
+        if(hoveredEl.offsetTop <= hoveredElPosition[0] - 100 ||
+            hoveredEl.offsetTop >= hoveredElPosition[0] + 100 ||
+            hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 ||
+            hoveredEl.offsetLeft >= hoveredElPosition[1] + 100){
+            hoveredEl.style.cssText = "";
+            hoveredElPosition = [];
+        }
+
+        hoveredEl.onmouseleave = () => {
+            hoveredEl.style.cssText = "";
+            hoveredElPosition = [];
+        }
+    }
+}
+
 document.body.addEventListener("mousemove", e => { 
     let x = e.clientX;
     let y = e.clientY;
     mouseCircleFn(x,y);
     animateCircles(e, x, y);
+
+    // Sticky Element
+    const hoveredEl = document.elementFromPoint(x, y);
+    
+    stickyElement(x, y, hoveredEl);
+    
+    // End of Sticky ELement
  });
 
  document.body.addEventListener("mouseleave", () => {
